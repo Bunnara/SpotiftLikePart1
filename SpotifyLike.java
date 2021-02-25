7354cf9087f5c6c540299cbe85d08e9f5b830d8a
Bunnara <bunnaras@yahoo.com>
2021-02-24 21:57:40 -0800

    Part 2 of the SpotifyLike App
    Contain the following commands:
    Play,
    Pause,
    Fast Forward,
    Rewind

--- SpotifyLike.java
+++ SpotifyLike.java
@@ -1,5 +1,8 @@
 import java.io.*;
 import java.util.*;
+import java.time.Duration;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import com.mpatric.mp3agic.ID3v1;
 import com.mpatric.mp3agic.ID3v1Tag;
 import com.mpatric.mp3agic.ID3v2;
@@ -11,35 +14,152 @@ import com.mpatric.mp3agic.UnsupportedTagException;
 
 import javazoom.jl.decoder.*;
 import javazoom.jl.player.*;
+import javazoom.jl.player.advanced.*;
+import javazoom.jl.player.advanced.jlap.*;
 
+class IncrementTimerEverySec extends TimerTask {
+	public void run() {
+		if(PlayAudioFile.pauseFlag == true) {
+          PlayAudioFile.currentLength = PlayAudioFile.currentLength;
+		} else {
+		  PlayAudioFile.currentLength += 30;
+	    }
+	}
+}
 
-
-class PlayAudioFile extends Thread {
+class PlayAudioFile extends AdvancedPlayer implements Runnable {
    public File[] arrayOfMusic;
-   jlp music = null;
+   AdvancedPlayer ap;
+   static int songId = -1;
+   jlap music = null;
    int cMusicIndex = 0;
-   public PlayAudioFile() {
-	 try {
-       File musicFiles = new File("./audio");
-       this.arrayOfMusic = musicFiles.listFiles();
-      } catch(Exception e) {
-		  System.out.println(e);
+   int songPos = 0;
+   static boolean pauseFlag = false;
+   static int currentLength = 0;
+   static FileInputStream fis;
+   static AdvancedPlayer klassAdPlay = null;
+   static char[] favoriteList = new char[1050];
+
+   public PlayAudioFile(InputStream stream) throws JavaLayerException {
+	   super(stream);
+   }
+   public PlayAudioFile(InputStream stream, AudioDevice device) throws JavaLayerException {
+	   super(stream, device);
+   }
+
+   public PlayAudioFile() throws FileNotFoundException, JavaLayerException {
+      super(new FileInputStream("audio/Aglow Hollow - Winnowing Moon (live).mp3"));
+   }
+
+   public void init() {
+	   try {
+	     File musicFiles = new File("./audio");
+	     this.arrayOfMusic = musicFiles.listFiles();
+	   } catch(Exception e) {
+	   	 System.out.println(e);
       }
    }
 
    public void run() {
-      System.out.println("Do you Like the song?");
-      Scanner scanner = new Scanner(System.in);
-      String likeSong = scanner.nextLine();
-      if(likeSong.contains("yes")) {
-		  System.out.println("You like this song.");
+	   try {
+		 if(PlayAudioFile.pauseFlag == true) {
+		   pauseCurrently();
+		 } else {
+           playCurrently();
+	     }
+	   } catch(JavaLayerException e) {
+		 e.printStackTrace();
+	   }
+   }
+
+   public void pauseCurrently() throws JavaLayerException {
+	     klassAdPlay = this.playMusic(songId, PlayAudioFile.currentLength);
+	     klassAdPlay.play(PlayAudioFile.currentLength, PlayAudioFile.currentLength);
+   }
+
+   public void playCurrently() throws JavaLayerException {
+	     klassAdPlay = this.playMusic(songId, PlayAudioFile.currentLength);
+	     klassAdPlay.play(PlayAudioFile.currentLength, 120000);
+   }
+
+   public static void destroyMusicalThread(Thread k) {
+      if((k.getName() == "music")) {
+		  k.stop();
 	  }
+   }
+
+   public void promptWhenPlay() throws JavaLayerException, InterruptedException, IOException {
+	  System.out.println("Do you Like the song?");
+	  Scanner scanner = new Scanner(System.in);
+	  String likeSong = scanner.nextLine();
+	   if(likeSong.contains("yes")) {
+	   	   System.out.println("You like this song.");
+	   } else if(likeSong.contains("Forward")) {
+		   PlayAudioFile.currentLength += 150;
+		   screenRefresh();
+		   System.out.println("Fast Forwarding");
+   	  } else if(likeSong.contains("Rewind")) {
+		   PlayAudioFile.currentLength -= 150;
+		   screenRefresh();
+		   System.out.println("Rewinding");
+   	  } else if(likeSong.contains("Pause")) {
+		   PlayAudioFile.pauseFlag = true;
+		   screenRefresh();
+		   System.out.println("Pausing");
+   	  } else if(likeSong.contains("Play")) {
+		   PlayAudioFile.pauseFlag = false;
+		   screenRefresh();
+		   System.out.println("Playing");
+   	  }  else if(likeSong.contains("Favorite")) {
+		   favoriteChoice(true);
+		   screenRefresh();
+		   System.out.println("You Favorite this song.");
+   	  }  else if(likeSong.contains("Unfavorite")) {
+		   favoriteChoice(false);
+           screenRefresh();
+		   System.out.println("You Un-Favorite this song.");
+   	  } else if(likeSong.contains("back")) {
+		  try {
+			 screenRefresh();
+   	         FrontPage fpg = new FrontPage();
+   	         fpg.display();
+   	       } catch(IOException e) {
+		   	 e.printStackTrace();
+		   } catch(InterruptedException e) {
+		   	 e.printStackTrace();
+	       }
+   	  } else {
+		   screenRefresh();
+	  }
+   }
+
+   public void screenRefresh() throws InterruptedException, IOException {
+	    Map<Thread, StackTraceElement[]> collectionOfThreads = Thread.getAllStackTraces();
+	    collectionOfThreads.forEach((k, v) -> destroyMusicalThread(k));
+		new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+   }
+
+   public void favoriteChoice(boolean flag) throws IOException {
+	        String nameOfMusic = this.arrayOfMusic[cMusicIndex].getPath().replace(".mp3", "").replace(".\\audio\\", "");
+	        String newFavoriteList = "";
+	        String numWriteToFile = "";
+	        numWriteToFile = String.valueOf(cMusicIndex) + "]" + nameOfMusic + "\n";
+			newFavoriteList += numWriteToFile;
+			if(flag == false) {
+				newFavoriteList = (new String(favoriteList)).replace(numWriteToFile, "");
+			}
+            FileWriter fileW = new FileWriter("./audio/favorite/fav.txt");
+			fileW.write(newFavoriteList);
+	   	    fileW.close();
    }
 
    public void getInfo(int musicIndex) {
 	 try {
+	 FileReader fileR = new FileReader("./audio/favorite/fav.txt");
+	 fileR.read(favoriteList, 0, 1050);
+	 fileR.close();
 	 cMusicIndex = musicIndex;
-	 this.music = new jlp((this.arrayOfMusic[cMusicIndex]).getPath());
+	 this.music = new jlap();
 	 Mp3File mp3file = new Mp3File((this.arrayOfMusic[cMusicIndex]).getPath());
 	 ID3v2 id3v2Tag = mp3file.getId3v2Tag();
 	 System.out.println("Title: " + id3v2Tag.getTitle());
@@ -47,6 +167,14 @@ class PlayAudioFile extends Thread {
 	 System.out.println("Year: " + id3v2Tag.getYear());
 	 System.out.println("Genre: " + id3v2Tag.getGenre());
 	 System.out.println("File Path: " + this.arrayOfMusic[cMusicIndex].getPath());
+	 Pattern musicNum = Pattern.compile(String.valueOf(cMusicIndex) + "]" + this.arrayOfMusic[cMusicIndex].getPath().replace(".mp3", "").replace(".\\audio\\", ""), Pattern.CASE_INSENSITIVE);
+	 Matcher favFlagTrue = musicNum.matcher(new String(favoriteList));
+	 boolean favFound = favFlagTrue.find();
+	 if(Boolean.valueOf(favFound).equals(Boolean.TRUE)) {
+	     System.out.println("isFavorite: true");
+	 } else if(Boolean.valueOf(favFound).equals(Boolean.FALSE)){
+		 System.out.println("isFavorite: false");
+     }
 	 } catch(IOException e) {
 	   e.printStackTrace();
 	 } catch(UnsupportedTagException e) {
@@ -55,13 +183,17 @@ class PlayAudioFile extends Thread {
 	   e.printStackTrace();
 	 }
    }
-   public void play(int musicIndex) {
+   public AdvancedPlayer playMusic(int musicIndex, int start) {
 	 this.getInfo(musicIndex);
 	 try {
-	    this.music.play();
+		PlayAudioFile.fis = new FileInputStream(this.arrayOfMusic[cMusicIndex].getPath());
+	    this.ap = new AdvancedPlayer(PlayAudioFile.fis);
 	 } catch(JavaLayerException e) {
 	    e.printStackTrace();
+	 } catch(IOException e) {
+		e.printStackTrace();
 	 }
+	 return this.ap;
    }
 
    public int findIndexName(String name) {
@@ -98,20 +230,32 @@ class LoadTextFromFile {
 
 class HomeMenu {
   public HomeMenu() {
-     PlayAudioFile music = new PlayAudioFile();
-     music.displayMusicNames();
      System.out.println("Do you want to add a song to the Library?");
      try {
-	   Scanner input = new Scanner(System.in);
+       PlayAudioFile music = new PlayAudioFile();
+       music.init();
+       music.displayMusicNames();
+	   MusicInfo musicInfo = new MusicInfo();
 	   FileWriter libraryFile = new FileWriter("./library.txt");
 	   String line = "";
-	   while(!line.contains("quit")) {
-		  line.replace("quit;", "");
+	   while(!line.contains("done")) {
 		  libraryFile.write(line);
-		  line = input.nextLine() + ";";
+		  line = String.valueOf(musicInfo.getIndexOfMusic(true, null)) + ") " + musicInfo.musicName + "\n";
+		  if(line.contains("done")) {
+		  line.replace("done", "");
+		  libraryFile.close();
+          FrontPage fpg = new FrontPage();
+            try {
+              fpg.display();
+              fpg.nextScreen();
+		    } catch(InterruptedException e) {
+				e.printStackTrace();
+		    }
+		}
        }
-       libraryFile.close();
      } catch(IOException e) {
+		 e.printStackTrace();
+	 } catch(JavaLayerException e) {
 		 e.printStackTrace();
 	 }
   }
@@ -120,37 +264,100 @@ class SearchByTitleMenu {
    public SearchByTitleMenu() {
 
    }
+
    public void printRequest() {
 	   System.out.println("Please Enter a Song By Title:");
    }
-   public void searchSong() {
-      PlayAudioFile paf = new PlayAudioFile();
-      Scanner input = new Scanner(System.in);
-      String musicName = input.nextLine();
-      paf.start();
-      paf.play(paf.findIndexName(musicName));
+
+   public void searchSong() throws JavaLayerException, FileNotFoundException, InterruptedException, IOException {
+	 MusicInfo musicInfo = new MusicInfo();
+     int indexOfAudioFile = musicInfo.getIndexOfMusic(true, null);
+     SelectMusic selectedMusic = new SelectMusic();
+     selectedMusic.playSelected(indexOfAudioFile);
    }
 }
+
+class SelectMusic {
+	public SelectMusic() throws InterruptedException, IOException {
+       new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+	}
+	public void playSelected(int id) throws JavaLayerException, FileNotFoundException, InterruptedException, IOException {
+        do {
+	     Timer timer = new Timer();
+	     timer.schedule(new IncrementTimerEverySec(), 1000, 1000);
+		  PlayAudioFile paf = new PlayAudioFile();
+		  paf.init();
+		  PlayAudioFile.songId = id;
+		  Thread pafRunnable = new Thread(paf, "music");
+		  pafRunnable.start();
+          paf.promptWhenPlay();
+	    } while(true);
+	}
+}
+
+class MusicInfo {
+	String musicName;
+
+	public MusicInfo() {
+
+	}
+	public int getIndexOfMusic(boolean askForNameOfMusic, String nameOfMusic) {
+      int indexOfAudioFile = 0;
+      if(askForNameOfMusic == true) {
+	     Scanner input = new Scanner(System.in);
+         musicName = input.nextLine();
+	  } else {
+		 musicName = nameOfMusic;
+	  }
+      File audioFile = new File("./audio");
+      File[] audioFiles = audioFile.listFiles();
+      for(int i = 0; i < audioFiles.length; i++) {
+		  if(musicName.equals(audioFiles[i].getName().replace(".mp3", ""))) {
+            indexOfAudioFile = i;
+		  }
+	  }
+	  return indexOfAudioFile;
+	}
+}
+
 class LibraryMenu {
-    public LibraryMenu() throws IOException {
-	  PlayAudioFile paf = new PlayAudioFile();
-      FileReader libraryFile = new FileReader("./library.txt");
-      char[] textToBeRead = new char[1000];
-      libraryFile.read(textToBeRead, 0, 1000);
-      String textFromFile = "";
-      for(char eachChar: textToBeRead) {
-		  textFromFile += eachChar;
-	  }
-      String[] arrayOfMusicInLibrary = textFromFile.split(";");
-      for(short i = 0; i < arrayOfMusicInLibrary.length; i++) {
-		System.out.println(paf.findIndexName(arrayOfMusicInLibrary[i]) + ") " + arrayOfMusicInLibrary[i]);
-	  }
-      libraryFile.close();
-      System.out.println("Select a song to play by number: ");
-      Scanner scanner = new Scanner(System.in);
-      short num = scanner.nextShort();
-      scanner.close();
-      paf.play(num);
+    public LibraryMenu() throws IOException, JavaLayerException, InterruptedException {
+	    MusicDisplayer fileToReadFrom = new MusicDisplayer("./library.txt");
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        SelectMusic selectedMusic = new SelectMusic();
+        selectedMusic.playSelected(num);
+	}
+}
+
+class MusicDisplayer {
+	public MusicDisplayer(String fileName) throws IOException, JavaLayerException, FileNotFoundException, InterruptedException {
+	   PlayAudioFile paf = new PlayAudioFile();
+	   paf.init();
+	   FileReader musicNamesInFile = new FileReader(fileName);
+	   char[] textToBeRead = new char[1000];
+	   musicNamesInFile.read(textToBeRead, 0, 1000);
+	   String textFromFile = "";
+	   for(char eachChar: textToBeRead) {
+		   textFromFile += eachChar;
+	   }
+	   String[] arrayOfFavorites = textFromFile.split("\n");
+	   for(short i = 0; i < arrayOfFavorites.length; i++) {
+		 System.out.println(arrayOfFavorites[i]);
+	   }
+       musicNamesInFile.close();
+	}
+}
+
+class FavoriteMenu {
+	public FavoriteMenu() throws IOException, JavaLayerException, InterruptedException {
+		System.out.println("Choose a song from your favorite list to play.");
+        System.out.println("Select a song to play by number: ");
+        MusicDisplayer fileToReadFrom = new MusicDisplayer("./audio/favorite/fav.txt");
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        SelectMusic selectedMusic = new SelectMusic();
+        selectedMusic.playSelected(num);
 	}
 }
 
@@ -159,7 +366,6 @@ class FrontPage {
 
 	}
 	public void nextScreen() {
-       Runtime process = Runtime.getRuntime();
        try {
        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
        } catch(IOException e) {
@@ -168,7 +374,7 @@ class FrontPage {
 		   e.printStackTrace();
 	   }
 	}
-	public void display() {
+	public void display() throws IOException, InterruptedException {
 	  LoadTextFromFile txt = new LoadTextFromFile();
       txt.outputMenus();
       InputStream is = System.in;
@@ -183,7 +389,11 @@ class FrontPage {
 		    nextScreen();
 		    SearchByTitleMenu musicTitle = new SearchByTitleMenu();
 		    musicTitle.printRequest();
-		    musicTitle.searchSong();
+		    try {
+		      musicTitle.searchSong();
+		    } catch(JavaLayerException e) {
+			  e.printStackTrace();
+			}
 		    break;
 		case "l":
 		    nextScreen();
@@ -191,17 +401,23 @@ class FrontPage {
 		      LibraryMenu libMenu = new LibraryMenu();
 		    } catch(IOException e) {
 				e.printStackTrace();
+			} catch(JavaLayerException e) {
+				e.printStackTrace();
 			}
 		    break;
+		case "f":
+		    nextScreen();
+		    try {
+		      FavoriteMenu favMenu = new FavoriteMenu();
+		    } catch(IOException e) {
+				e.printStackTrace();
+			} catch(JavaLayerException e) {
+				e.printStackTrace();
+			}
+		case "Q":
 		case "q":
-		    Runtime process = Runtime.getRuntime();
-			try {
-			    new ProcessBuilder("cmd", "/c", "exit").inheritIO().start().waitFor();
-			} catch(IOException e) {
-				e.printStackTrace();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-	        }
+		    nextScreen();
+			System.exit(0);
 		    break;
 	  }
 	}
@@ -209,7 +425,13 @@ class FrontPage {
 
 class SpotifyLike {
 	public static void main(String[] args) {
-       FrontPage fpg = new FrontPage();
-       fpg.display();
+		try {
+          FrontPage fpg = new FrontPage();
+          fpg.display();
+	    } catch(IOException e) {
+			e.printStackTrace();
+		} catch(InterruptedException e) {
+		    e.printStackTrace();
+	    }
 	}
 }
